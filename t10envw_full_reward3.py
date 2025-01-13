072d963f7d12c3fbcb1d1995a2c3d7f304e74eff
root <232345@buas.nl>
2025-01-13 14:05:53 +0000

    approach 4, less big numbers, also more punishment

---
+++ t10envw_full_reward3.py
@@ -0,0 +1,119 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+from sim_class import Simulation
+import random
+import math
+import rl_functions as do
+
+
+class OT2Env(gym.Env):
+    """Custom Environment that follows gym interface."""
+    def __init__(self, render=False, max_steps=1000):
+        super(OT2Env, self).__init__()
+
+        # Initialize the simulation
+        self.sim_sim = Simulation(num_agents=1, render=False)
+
+        self.x_min = do.find_limit(self.sim_sim, axis=0, direction=-1, silence=True)
+        self.x_max = do.find_limit(self.sim_sim, axis=0, direction=1, silence=True)
+        self.y_min = do.find_limit(self.sim_sim, axis=1, direction=-1, silence=True)
+        self.y_max = do.find_limit(self.sim_sim, axis=1, direction=1, silence=True)
+        self.z_min = do.find_limit(self.sim_sim, axis=2, direction=-1, silence=True)
+        self.z_max = do.find_limit(self.sim_sim, axis=2, direction=1, silence=True)
+
+        self.sim_sim.close()
+
+        self.sim = Simulation(num_agents=1, render=render)
+
+        # Define variables
+        self.render = render
+        self.max_steps = max_steps
+        self.steps = 0
+        self.pipette_offset = self.sim.pipette_offset
+
+        # Action space: controlling pipette in x, y, z directions
+        self.action_space = spaces.Box(low=np.array([-1, -1, -1]), high=np.array([1, 1, 1]),
+                                            shape=(3,), dtype=np.float32)
+
+        # Observation space: pipette position (x, y, z) and goal position (x, y, z)
+        self.observation_space = spaces.Box(low=-math.inf, high=math.inf,
+                                            shape=(6,), dtype=np.float32) # for one robot
+
+    # RESET
+    def reset(self, seed=None, options=None):
+        if seed is not None:
+            np.random.seed(seed)
+        
+        self.goal_position = [random.uniform(self.x_min, self.x_max), random.uniform(self.y_min, self.y_max), random.uniform(self.z_min, self.z_max)]
+        
+        # r2 - Info (+ reset)
+        info = self.sim.reset(num_agents=1)
+
+        # r1 - Observation
+        first_robot_key = list(info.keys())[0]
+        pipette_position = np.array(info[first_robot_key]["pipette_position"], dtype=np.float32)
+        observation = np.concatenate((pipette_position, self.goal_position)).astype(np.float32)
+
+        # Reset variables
+        self.steps = 0
+
+        return observation, info
+
+    # STEP
+    def step(self, action):
+        
+        # Append 0 for the drop action (since we're controlling only 3 actions: x, y, z)
+        action = np.append(action, 0)  # appending 0 for drop action
+
+        # r5 - Info (+ run action)
+        info = self.sim.run([action]) # expects list of actions [[x,y,z,drop], [x,y,z,drop], ...]
+
+        # r1 - Observation
+        first_robot_key = list(info.keys())[0]
+        pipette_position = np.array(info[first_robot_key]["pipette_position"], dtype=np.float32)
+        observation = np.concatenate((pipette_position, self.goal_position)).astype(np.float32)
+
+        # r2 - Reward
+        distance_to_goal = np.linalg.norm(pipette_position - self.goal_position)
+        if hasattr(self, 'previous_distance'):
+            distance_reward = (self.previous_distance - distance_to_goal)
+        else:
+            distance_reward = 0
+        self.previous_distance = distance_to_goal
+
+        # main reward -> negate the distance to goal
+        reward = -distance_to_goal
+        # -> add bonus/penalty if closer to goal than before or not
+        if distance_reward <= 0:
+            reward += distance_reward
+        else:
+            reward += distance_reward
+        # -> add penalty for taking a step
+        reward -= 0.01
+
+        # r3 - Terminated
+        threshold = 0.001
+        if distance_to_goal < threshold: # if task has been completed
+            reward += 10  # Positive reward for success
+            print(f'Treshold: {threshold}, Distance to goal: {distance_to_goal}')
+            terminated = True
+        else:
+            terminated = False
+        
+        # r4 - Truncated
+        if self.steps >= self.max_steps:
+            truncated = True
+        else:
+            truncated = False
+        self.steps += 1
+
+        return observation, reward, terminated, truncated, info
+
+    # RENDER
+    def render(self, mode="human"):
+        pass
+
+    # CLOSE
+    def close(self):
+        self.sim.close()
---
+++ t11train_full_reward3.py
@@ -0,0 +1,71 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import time
+import numpy as np
+from stable_baselines3 import PPO
+from t10envw_full_reward3 import OT2Env
+import wandb
+from wandb.integration.sb3 import WandbCallback
+import argparse
+from clearml import Task
+import sys
+import gymnasium as gym
+import tensorflow
+from typing_extensions import TypeIs
+
+# Env
+env = OT2Env()
+
+# ClearML
+task = Task.init(project_name="Mentor Group E/Group 3", task_name="OT2_RL_Training_APP4_")
+task.set_base_docker('deanis/2023y2b-rl:latest')
+task.execute_remotely(queue_name="default")
+
+# WandB
+os.environ['WANDB_API_KEY'] = '11c5fccd0b07e41fc8bef045f744781d2f777121'
+run = wandb.init(project="RL_train_more_reward",sync_tensorboard=True)
+
+# Arg parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("--learning_rate", type=float, default=0.0003)
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--n_steps", type=int, default=2048)
+parser.add_argument("--n_epochs", type=int, default=10)
+parser.add_argument("--gamma", type=float, default=0.99)
+parser.add_argument("--clip_range", type=float, default=0.2)
+args = parser.parse_args()
+
+# Define PPO
+model = PPO(
+    'MlpPolicy',
+    env,
+    verbose=1, 
+    learning_rate=args.learning_rate, 
+    batch_size=args.batch_size, 
+    n_steps=args.n_steps, 
+    n_epochs=args.n_epochs,
+    gamma=args.gamma,
+    tensorboard_log=f"runs/{run.id}",
+)
+
+
+# Classic Callback
+wandb_callback = WandbCallback(
+    model_save_freq = 1000,
+    model_save_path = f"models/{run.id}",
+    verbose = 2
+)
+
+model_dir = f"APP4_models/{run.id}"
+os.makedirs(model_dir, exist_ok=True)
+
+# Train
+n_of_times = 20
+timesteps = 400_000
+for i in range(n_of_times):
+    model.learn(total_timesteps=timesteps, callback=wandb_callback, reset_num_timesteps=False, progress_bar=True, tb_log_name=f"runs/{run.id}")
+    model_path = f"{model_dir}/{timesteps * (i + 1)}"
+
+    # Save ClearML
+    model.save(model_path)
+    task.upload_artifact(f"model_{timesteps * (i + 1)}", artifact_object=model_path)
