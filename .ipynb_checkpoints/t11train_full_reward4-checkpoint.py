7f963743d502c5b13f483b79995a686aa57c4a5c
root <232345@buas.nl>
2025-01-16 10:07:14 +0000

    trying to save model

--- .ipynb_checkpoints/t11train_full_reward4-checkpoint.py
+++ .ipynb_checkpoints/t11train_full_reward4-checkpoint.py
@@ -22,7 +22,7 @@ task.execute_remotely(queue_name="default")
 
 # WandB
 os.environ['WANDB_API_KEY'] = '11c5fccd0b07e41fc8bef045f744781d2f777121'
-run = wandb.init(project="RL_train_more_reward_v3",sync_tensorboard=True)
+run = wandb.init(project="RL_train_more_reward_v4",sync_tensorboard=True)
 
 # Create dir to store models
 model_dir = f"models/{run.id}"
@@ -31,9 +31,9 @@ os.makedirs(model_dir, exist_ok=True)
 # Arg parsing
 parser = argparse.ArgumentParser()
 parser.add_argument("--learning_rate", type=float, default=0.0001)
-parser.add_argument("--batch_size", type=int, default=64)
-parser.add_argument("--n_steps", type=int, default=2048)
-parser.add_argument("--n_epochs", type=int, default=30)
+parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--n_steps", type=int, default=4096)
+parser.add_argument("--n_epochs", type=int, default=50)
 parser.add_argument("--gamma", type=float, default=0.99)
 parser.add_argument("--clip_range", type=float, default=0.2)
 parser.add_argument("--vf_coef", type=float, default=0.5)
@@ -65,5 +65,6 @@ wandb_callback = WandbCallback(
 
 # Train
 model.learn(total_timesteps=args.timesteps, callback=wandb_callback, reset_num_timesteps=False, progress_bar=True, tb_log_name=f"runs/{run.id}")
-model.save(f"models/{run.id}/{args.timesteps}_baseline")
-wandb.save(f"models/{run.id}/{args.timesteps}_baseline")
+model_save_path = f"models/{run.id}/{args.timesteps}_baseline"
+wandb.save(model_save_path)
+task.upload_artifact(model_save_path)
